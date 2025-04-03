8b4ff6481bfcd7f9130d0061fe3b5c1b07763fa8
Vinicius Souza <viniciussilvasouza62@gmail.com>
2025-04-03 14:28:54 -0300

    Commit inicial

---
+++ veiculoPneu/src/main/java/com/project/veiculoPneu/VeiculoPneuApplication.java
@@ -0,0 +1,13 @@
+package com.project.veiculoPneu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculoPneuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculoPneuApplication.class, args);
+	}
+
+}
---
+++ veiculoPneu/src/test/java/com/project/veiculoPneu/VeiculoPneuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.veiculoPneu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculoPneuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
